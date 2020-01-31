ba00bdb1b42337c947474d65c03b71dc027a0213
Tatiana <eyesdwn@gmail.com>
2020-01-31 16:52:42 +0200

    Updates

---
+++ precache-manifest.ec2464388f9d014dddead8435b00e5db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe15c433796374616590859db9cbad2",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "1e6dd9d5be244b46d208",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.42428574.chunk.css"
+  },
+  {
+    "revision": "f37fab53ef0c13c73b67",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d2cb301d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d2cb301d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6dd9d5be244b46d208",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.de6dd330.chunk.js"
+  },
+  {
+    "revision": "087bead6dde9c43ad19e",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.c604e018.js"
+  }
+]);
